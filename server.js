5d43b356ffb37dcd1b22aa56219f975c43ad2ea4
Projectguide <r.devi@gct.ac.in>
2017-08-21 15:54:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,52 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var articleOne = {
+    title: 'articleone Devi',
+    heading:'Articleone',
+    data: 'aug 21 2017',
+    content: `<p>
+                this is the html page article twolinking to the server.js file
+                 this is the html page linking to the server.js file
+            </p>
+             <p>
+                this is the html page article twolinking to the server.js file
+                 this is the html page linking to the server.js file
+            </p>
+             <p>
+                this is the html page article twolinking to the server.js file
+                 this is the html page linking to the server.js file
+            </p>`
+};
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmltemplate = `<html>
+    <head>
+        <title>
+            $title
+        </title>
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <h2>
+        $heading
+        </h2>
+        <div>
+        $date
+        </div>
+        <div class="container">
+            <a href="/">Home</a>
+        
+        <div>
+            $content
+        </div>
+        </div>
+    </body>
+</html>`;
+}
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
