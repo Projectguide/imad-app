0394f50da024610b3c10a1f08adb9d84fc4b678f
Projectguide <r.devi@gct.ac.in>
2017-08-21 15:58:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-three', function (req, res) {
